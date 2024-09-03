8bb87073673bf480d631a51a56bdec8e0a87d026
Revenge-of-Shadow <vyacheslav.pigida@gmail.com>
2024-09-03 22:28:00 +0300

    Sevent commit. It works. IT WOOOOOOOOOOOOOOOOORKS!

--- PS_Primary_board/PS_Primary_board.ino
+++ PS_Primary_board/PS_Primary_board.ino
@@ -1,5 +1,12 @@
- 
-#define DPin_Signal       9 
+ //      Sending digital pins.
+#define     DPin_Justice  8
+#define     DPin_finish   9
+#define     DPin_move     10
+#define     DPin_scan     11
+#define     DPin_shoot    12
+#define     DPin_spot     13
+//      Sending digital pins end.
+
 #define DPin_Light        7
 #define DPin_Echo         6
 #define DPin_Trigger      5
@@ -7,11 +14,12 @@
 #define DPin_SRotation    3
 #define DPin_STilt        2
 
+#define servo_pause       20
+
 #include <SPI.h>
 #include <SD.h>
 #include <string.h>
 #include <Servo.h>
-   // create an object for use in this sketch
 
 
 Servo sRotation;
@@ -32,22 +40,76 @@ enum tracks{
   spot2
 };
 
-void rotateServoR(int degree, int pause){
+const char* filenames [12] = {      //  All of the filenames. 
+  "",
+  "Justice.wav",
+  "finish.wav",
+  "move.wav",
+  "scan.wav",
+  "scan2.wav",
+  "scan3.wav",
+  "shoot.wav",
+  "shoot2.wav",
+  "shoot3.wav",
+  "spot.wav",
+  "spot2.wav"
+};
+
+const int delays [12] = {             //  All of the corresponding delays.
+  0,      //  none
+  120000, //  Justice
+  500,    //  finish
+  500,    //  move
+  400,    //  scan
+  400,    //  scan2
+  400,    //  scan3
+  400,    //  shoot
+  400,    //  shoot2
+  400,    //  shoot3
+  200,    //  spot
+  200     //  spot2
+
+};
+
+void rotateServoR(int degree){
   sRotation.write(degree);
-  delay(pause);
+  delay(servo_pause);
 }
-void rotateServoT(int degree, int pause){
+void rotateServoT(int degree){
   sTilt.write(degree);
-  delay(pause);
+  delay(servo_pause);
 }
 
+void  signalSend(int pin, int duration){
+  Serial.print(pin);
+  Serial.print(" On...");
+  digitalWrite(pin, HIGH);
+  delay(duration);
+  digitalWrite(pin, LOW);
+  Serial.println(" Off.");
+}
 
 
 void setup(){
   //        Pure initialization.
+
+  pinMode(DPin_Justice, OUTPUT);
+  pinMode(DPin_finish,  OUTPUT);
+  pinMode(DPin_move,    OUTPUT);
+  pinMode(DPin_scan,    OUTPUT);
+  pinMode(DPin_shoot,   OUTPUT);
+  pinMode(DPin_spot,    OUTPUT);
+
+  digitalWrite(DPin_Justice, LOW);
+  digitalWrite(DPin_finish,  LOW);
+  digitalWrite(DPin_move,    LOW);
+  digitalWrite(DPin_scan,    LOW);
+  digitalWrite(DPin_shoot,   LOW);
+  digitalWrite(DPin_spot,    LOW);
+  
+
   pinMode(DPin_Echo, INPUT);
   pinMode(DPin_Trigger, OUTPUT);
-  pinMode(DPin_Signal, OUTPUT);
   sRotation.attach(DPin_SRotation);
   sTilt.attach(DPin_STilt);
   digitalWrite(DPin_Light, HIGH);
@@ -55,9 +117,7 @@ void setup(){
   Serial.begin(9600);
 
   //        Action.
-  analogWrite(DPin_Signal, move);
-
-
+  signalSend(DPin_move, delays[move]);
 }
 
 
@@ -66,13 +126,11 @@ bool  subtract_degrees  = true;
 bool  alarmed           = false;
 float distance;
 void loop(){ 
-  rotateServoR(hor_degrees, 50);
-  
   //    Disabled state.
   if(!digitalRead(DPin_Switch))
   {
-    rotateServoR(90, 100);
-    rotateServoT(60, 100);
+    rotateServoR(90);
+    rotateServoT(60);
     while(!digitalRead(DPin_Switch)){
       delay(500);
       digitalWrite(DPin_Light, LOW);
@@ -81,71 +139,72 @@ void loop(){
     }
 
     
-    rotateServoT(90, 100);
-    rotateServoR(90, 100);
-
+    rotateServoT(90);
+    rotateServoR(90);
     
-    
-    Serial.println("Setup finished.");
+    signalSend(DPin_finish, delays[finish]);
   }
-
-
-  //  Main loop.
+  //    Disabled state end.
   
-  //  Check for the distance and set alarmed.
-    digitalWrite(DPin_Trigger, LOW);
-    delayMicroseconds(5);
-    digitalWrite(DPin_Trigger, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(DPin_Trigger, LOW);
 
-    long duration = pulseIn(DPin_Echo, HIGH, 5000L);
-    // Convert the time into a distance
-    distance = duration*0.034/2;
+  else{
+    rotateServoR(hor_degrees);
+    //  Main loop.
+    
+    //  Check for the distance and set alarmed.
+      digitalWrite(DPin_Trigger, LOW);
+      delayMicroseconds(5);
+      digitalWrite(DPin_Trigger, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(DPin_Trigger, LOW);
+
+      long duration = pulseIn(DPin_Echo, HIGH, 5000L);
+      // Convert the time into a distance
+      distance = duration*0.034/2;
 
 
-  if(distance < 100.0 && distance > 40){  //  Distance below normal, become alarmed.
-    if(!alarmed){ //  The sound is played only on the moment of becoming alarmed.
-      analogWrite(DPin_Signal, spot);
+    if(distance < 100.0 && distance > 40){  //  Distance below normal, become alarmed.
+      if(!alarmed){ //  The sound is played only on the moment of becoming alarmed.
+        signalSend(DPin_spot, delays[spot]);
+      }
+      alarmed = true;
     }
-    alarmed = true;
-  }
-  else if(distance){
-    alarmed = false;
-  }
-  
-  if(alarmed){  //  Attack as long as alarmed.
+    else if(distance){
+      alarmed = false;
+    }
+    
+    if(alarmed){  //  Attack as long as alarmed.
+      rotateServoT(100);
+      signalSend(DPin_shoot, delays[shoot]);
+      rotateServoT(90);
+    }
+    else{         //  Not alarmed, proceed with sweeping.
 
-    analogWrite(DPin_Signal, shoot);
-    rotateServoT(100, 100);
-    rotateServoT(90, 100);
-  }
-  else{         //  Not alarmed, proceed with sweeping.
-
-      if(subtract_degrees){       //  Going left.
-        if(hor_degrees == 0){     //  Can not go left anymoe. Reversing.
-          subtract_degrees = false;
+        if(subtract_degrees){       //  Going left.
+          if(hor_degrees == 0){     //  Can not go left anymoe. Reversing.
+            subtract_degrees = false;
+            ++hor_degrees;          //  Save a tick, exclude a bug.
+          }
+          else{                     //  Turning left as planned.
+            --hor_degrees;
+          }
         }
-        else{                     //  Turning left as planned.
-          --hor_degrees;
+        else{                       //  Going right.
+          if(hor_degrees == 180){   //  Can not go right anymoe. Reversing.
+            subtract_degrees = true;
+            --hor_degrees;          //  Save a tick, exclude a bug.
+          }
+          else{                     //  Turning right as planned.
+            ++hor_degrees;
+          }
         }
-      }
-      else{                       //  Going right.
-        if(hor_degrees == 180){   //  Can not go right anymoe. Reversing.
-          subtract_degrees = true;
-        }
-        else{                     //  Turning right as planned.
-          ++hor_degrees;
-        }
-      }
 
-      if(hor_degrees%30 == 0){
-        analogWrite(DPin_Signal, scan);
-      }
+        if(hor_degrees%90 == 0){
+          signalSend(DPin_scan, delays[scan]);
+        }
 
+    }
   }
 
-
-  delay(100);
 
 }
--- PS_Secondary_board/PS_Secondary_board.ino
+++ PS_Secondary_board/PS_Secondary_board.ino
@@ -2,9 +2,17 @@
 #include <pcmConfig.h>
 #include <pcmRF.h>
 
+//      Receiving digital pins.
+#define     DPin_Justice  2
+#define     DPin_finish   3
+#define     DPin_move     4
+#define     DPin_scan     5
+#define     DPin_shoot    6
+#define     DPin_spot     7
+//      Receiving digital pins end.
+
 #define     DPin_CS       8
 #define     DPin_Speaker  9
-#define     APin_Signal   0
 
 #define     length        12        //  Fixed amount of file entries, including NONE.
 
@@ -41,14 +49,14 @@ const char* filenames [12] = {      //  All of the filenames.
 const int delays [12] = {             //  All of the corresponding delays.
   0,      //  none
   120000, //  Justice
-  200,    //  finish
-  200,    //  move
-  200,    //  scan
-  200,    //  scan2
-  200,    //  scan3
-  200,    //  shoot
-  200,    //  shoot2
-  200,    //  shoot3
+  500,    //  finish
+  500,    //  move
+  400,    //  scan
+  400,    //  scan2
+  400,    //  scan3
+  400,    //  shoot
+  400,    //  shoot2
+  400,    //  shoot3
   200,    //  spot
   200     //  spot2
 
@@ -57,31 +65,74 @@ const int delays [12] = {             //  All of the corresponding delays.
 TMRpcm tmrpcm;
 
 void playAudio(const char* filename, int duration){
+  Serial.print(filename);
+  Serial.print(" : playing for ");
+  Serial.print(duration);
+  Serial.println(" milliseconds. ");
   if(!duration) return;
 
   tmrpcm.stopPlayback();  //  Paranoia.
   tmrpcm.play(filename);
   delay(duration);
   tmrpcm.stopPlayback();
+  digitalWrite(DPin_Speaker, HIGH);
+  Serial.println("Done.");
 }
 
 void setup() {  
-  if (!SD.begin(DPin_CS)) { 
+  Serial.begin(9600);
+  digitalWrite(DPin_Speaker, HIGH);
+  while (!SD.begin(DPin_CS)) { 
     Serial.println("SD fail");  
     delay(2000);
   }
+
+  pinMode(DPin_Justice, INPUT);
+  pinMode(DPin_finish,  INPUT);
+  pinMode(DPin_move,    INPUT);
+  pinMode(DPin_scan,    INPUT);
+  pinMode(DPin_shoot,   INPUT);
+  pinMode(DPin_spot,    INPUT);
 
   tmrpcm.speakerPin = DPin_Speaker;
   tmrpcm.setVolume(6);
 }
 
 void loop() {
-
-  int val = analogRead(APin_Signal);
-
-  if(val < length && val > 0){
-    playAudio(filenames[val], delays[val]);
+  
+  if(digitalRead(DPin_Justice)){
+    tmrpcm.play(filenames[Justice]);
+    tmrpcm.loop(true);
+    while(digitalRead(DPin_Justice))
+    {
+      delay(10);
+    }
+    tmrpcm.stopPlayback();
+    tmrpcm.loop(false);
+  }
+  else
+  if(digitalRead(DPin_finish)){
+    playAudio(filenames[finish], delays[finish]);
+  }
+  else
+  if(digitalRead(DPin_move)){
+    playAudio(filenames[move], delays[move]);
+  }
+  else
+  if(digitalRead(DPin_scan)){
+    int chosen = random(scan, scan3+1);
+    playAudio(filenames[chosen], delays[chosen]);
+  }
+  else
+  if(digitalRead(DPin_shoot)){
+    int chosen = random(shoot, shoot3+1); 
+    playAudio(filenames[chosen], delays[chosen]);
+  }
+  else
+  if(digitalRead(DPin_spot)){
+    playAudio(filenames[spot], delays[spot]);
   }
 
   digitalWrite(DPin_Speaker, HIGH);
+  delay(10);
 }
